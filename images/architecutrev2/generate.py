13533be526194dee1aaa187763d5a142383c0e98
JakobRathberger <55431465+JakobRathberger@users.noreply.github.com>
2023-03-06 14:23:01 +0000

    Deploying to gh-pages from  @ 7082a7bca78f4f7f48c56bf5c739cfc45eb415e2 🚀

---
+++ images/architecutrev2/generate.py
@@ -0,0 +1,45 @@
+from diagrams import Cluster, Diagram, Edge
+from diagrams.custom import Custom
+from diagrams.programming.framework import Angular
+
+with Diagram(show=False, direction='LR'):
+    with Cluster("LEO-IOT"):
+        leo_backend = Custom("Backend", "quarkus-logo.jpg")
+
+        leo_frontend = Angular("Frontend")
+
+
+
+
+        graphana = Custom("Graphana", "grafana_logo.png")
+        leo_frontend <<  graphana
+        leo_frontend >>  leo_backend
+        timescale = Custom("Database", "timescale_logo.png")
+        leo_backend >> timescale
+
+
+    with Cluster("Value-Sim"):
+        leo_sim = Custom("Value Simulator", "quarkus-logo.jpg")
+        postgres = Custom("Database", "postgres_logo.png")
+        leo_sim >> postgres
+
+    with Cluster("vm90"):
+        mosquitto = Custom("Mqtt-Broker", "mosquitto_logo.png")
+
+    with Cluster("5AHIF"):
+        leobox1 = Custom("LeoBox", "leobox.png")
+
+    with Cluster("4BHITM"):
+        leobox2 = Custom("LeoBox", "leobox.png")
+
+    leobox1 >>  mosquitto
+    leobox2 >>  mosquitto
+
+    mosquitto >>  leo_frontend
+    mosquitto >>  leo_backend
+
+    leo_sim >>  mosquitto
+
+    graphana << timescale
+
+
